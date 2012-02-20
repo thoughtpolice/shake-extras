d72152382c445497e9d521b4046840d5a410824b
Austin Seipp <mad.one@gmail.com>
2012-02-19 23:03:00 -0600

    beginning of fancier profiling output

--- Development/Shake/CLI.hs
+++ Development/Shake/CLI.hs
@@ -5,23 +5,29 @@
 -- 
 -- Maintainer  : mad.one@gmail.com
 -- Stability   : experimental
--- Portability : ???
+-- Portability : GHC probably
+-- 
+-- This module provides a convenient
+-- cmdargs style interface for controlling
+-- shake build options.
 -- 
 module Development.Shake.CLI
-       ( options --
-       , shake'  -- 
+       ( options -- :: ShakeOptions
+       , shake'  -- :: Rules () -> IO ()
        ) where
-import System.Console.CmdArgs as CA
 
+import Control.Monad (when)
 import Development.Shake as Shake
+import Development.Shake.Report as Shake
+import System.Console.CmdArgs as CA
 
 options :: ShakeOptions
 options 
   = ShakeOptions { shakeFiles     = ".shake" &= help "shake journal/db path"
                  , shakeThreads   = 1        &= help "threads to use for build" &= name "j" &= typ "NUM"
-                 , shakeVersion   = 1        &= help "build system ver; increment to rebuild"
+                 , shakeVersion   = 1        &= ignore
                  , shakeStaunch   = False    &= help "keep going after error"  &= name "k"
-                 , shakeDump      = False    &= help "dump profiling report"   &= name "prof"
+                 , shakeDump      = False    &= help "dump profiling report into report.html" &= name "prof"
                  , shakeLint      = False    &= help "run build system linter" &= name "lint"
                  , shakeVerbosity = Shake.Normal &= ignore
                  } &= verbosity
@@ -34,3 +40,6 @@ shake' r = do
     CA.Normal -> shake x r
     CA.Quiet  -> shake (x { shakeVerbosity = Shake.Quiet }) r 
     CA.Loud   -> shake (x  { shakeVerbosity = Shake.Loud }) r
+  
+  when (shakeDump x) $ do
+    Shake.buildReportTemplate ".shake.js" "report.html"
---
+++ Development/Shake/Report.hs
@@ -0,0 +1,47 @@
+-- |
+-- Module      : Development.Shake.Report
+-- Copyright   : (c) Austin Seipp 2012
+-- License     : BSD3
+-- 
+-- Maintainer  : mad.one@gmail.com
+-- Stability   : experimental
+-- Portability : GHC probably
+-- 
+-- This module provides a convenient
+-- cmdargs style interface for controlling
+-- shake build options.
+-- 
+module Development.Shake.Report
+       ( buildReportTemplate -- :: FilePath -> FilePath -> IO ()
+       ) where
+import Data.ByteString as B (ByteString, readFile)
+import Data.ByteString.Lazy as BL
+import Text.Hastache as H
+import Text.Hastache.Context as H
+
+import System.IO.Unsafe (unsafePerformIO)
+import System.FilePath
+import Paths_shake_extras (getDataFileName)
+
+-- | Path to extras directory so we can find the report template, etc.
+extrasDir :: FilePath
+extrasDir = unsafePerformIO $ getDataFileName "extras"
+{-# NOINLINE extrasDir #-}
+
+-- | Takes a filepath pointing to the Shake \".js\" profiling dump.
+-- Second path indicates the output file to write.
+buildReportTemplate :: FilePath -> FilePath -> IO ()
+buildReportTemplate _jsfile _outfile = do
+  js       <- B.readFile _jsfile
+  template <- B.readFile (extrasDir </> "report.html")
+  BL.writeFile _outfile =<< (hastacheStr H.defaultConfig template (context js))
+  return ()
+
+context :: Monad m => B.ByteString -> MuContext m
+context shakejs = H.mkStrContext $ \name -> case name of
+  "bootstrapcss"  -> MuVariable $ bdir </> "css" </> "bootstrap.min.css"
+  "bootstrapjs"   -> MuVariable $ bdir </> "js" </> "bootstrap.min.js"
+  "jquery"        -> MuVariable $ extrasDir </> "jquery-1.7.1.min.js"
+  "shakedump"     -> MuVariable $ shakejs
+  _               -> MuNothing
+ where bdir = extrasDir </> "bootstrap"
