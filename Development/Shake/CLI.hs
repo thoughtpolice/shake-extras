f96d4993383e2271125a0797b2723fe61bfa206c
Austin Seipp <mad.one@gmail.com>
2012-03-20 18:14:13 -0500

    Remove all the old reporting code, since it's part of Shake 0.2.8

--- Development/Shake/CLI.hs
+++ Development/Shake/CLI.hs
@@ -18,7 +18,6 @@ module Development.Shake.CLI
 import System.FilePath ((<.>))
 import Control.Monad (when)
 import Development.Shake as Shake
-import Development.Shake.Report as Shake
 import System.Console.CmdArgs as CA
 
 -- | A 'ShakeOptions' data structure with 
@@ -29,7 +28,7 @@ options
                  , shakeThreads   = 1        &= help "threads to use for build" &= name "j" &= typ "NUM"
                  , shakeVersion   = 1        &= ignore
                  , shakeStaunch   = False    &= help "keep going after error"  &= name "k"
-                 , shakeDump      = False    &= help "dump profiling report into report.html" &= name "prof"
+                 , shakeReport    = Nothing  &= help "dump profiling report" &= name "prof" &= typFile
                  , shakeLint      = False    &= help "run build system linter" &= name "lint"
                  , shakeVerbosity = Shake.Normal &= ignore
                  } &= verbosity
@@ -44,9 +43,4 @@ shakeWithArgs r = do
   case v of
     CA.Normal -> shake x r
     CA.Quiet  -> shake (x { shakeVerbosity = Shake.Quiet }) r 
-    CA.Loud   -> shake (x  { shakeVerbosity = Shake.Loud }) r
-  
-  when (shakeDump x) $ do
-    -- FIXME: custom output file?
-    putStrLn "note: creating build system report in 'report.html'"
-    Shake.buildReport (shakeFiles x <.> ".js") "report.html"
+    CA.Loud   -> shake (x { shakeVerbosity = Shake.Loud }) r
